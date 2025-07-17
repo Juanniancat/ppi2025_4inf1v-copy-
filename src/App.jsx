5dcec8fd5fae4b868c647ca6e27329bc6c56984d
Juanniancat <juan.g@escolar.ifrn.edu.br>
2025-07-17 20:01:21 +0000

    merge and commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import "./styles/theme.css";
 import "./styles/global.css";
 import { ProductList } from "./components/ProductList";
 import { Header } from "./components/Header";
-<<<<<<< HEAD
 import { useState } from "react";
-=======
 import { LuckyNumber } from "./components/LuckyNumber";
 import { ProductList } from "./components/ProductList";
->>>>>>> 2b847fd0fd1cfe1239ef19d961e3d0b8ae107666
 
 export default function App() {
   
@@ -20,12 +17,9 @@ export default function App() {
   return (
     //React Fragment
     <>
-<<<<<<< HEAD
       <Header cart={cart} />
       <ProductList addToCart={addToCart} />
-=======
     <ProductList />
->>>>>>> 2b847fd0fd1cfe1239ef19d961e3d0b8ae107666
     </>
   );
 }
