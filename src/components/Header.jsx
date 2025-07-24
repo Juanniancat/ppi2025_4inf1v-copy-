a807e770316fafb585b4f1ea0f66ef380325bd4e
Juanniancat <juan.g@escolar.ifrn.edu.br>
2025-07-24 01:13:55 +0000

    Desafio 6

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,27 @@
-import { ShoppingBasket } from "lucide-react";
+      
+            import { ShoppingBasket } from "lucide-react";
 import styles from "./Header.module.css";
 import { Link } from "react-router";
 
 export function Header({ cart }) {
   return (
     <div className={styles.container}>
-      <Link to="/" className={styles.link}>
-        <h1>TJA Megastore</h1>
-      </Link>
-      <Link to="/cart" className={styles.link}>
-        <div className={styles.cartInfo}>
-          <ShoppingBasket size={32} />
+      <div className={styles.containerImg}>
+        {/* ícone/esquerda */}
+      </div>
+            <Link to="/" className={styles.link}><h1>TJA Megastore</h1></Link>
+           <div className={styles.cartInfo}>
+        <Link to="/cart" className={styles.link}>
+          {/* ícone da loja/direita */}
+           <ShoppingBasket size={32} />
           <p>
             Total: ${" "}
-            {cart.reduce((total, product) => total + product.price, 0).toFixed(2)}
+            {cart
+              .reduce((total, product) => total + product.price, 0)
+              .toFixed(2)}
           </p>
-        </div>
       </Link>
+      </div>
     </div>
   );
 }
