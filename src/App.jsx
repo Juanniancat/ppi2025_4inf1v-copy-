8b4bd25c17bee2d8621c8af1d5b11e6be03d21fc
Juanniancat <juan.g@escolar.ifrn.edu.br>
2025-07-25 18:31:41 +0000

    Codigo atualizado 25/07

--- src/App.jsx
+++ src/App.jsx
@@ -2,48 +2,21 @@ import "./styles/theme.css";
 import "./styles/global.css";
 import { ProductList } from "./components/ProductList";
 import { Header } from "./components/Header";
-import { useState } from "react"; 
 import { Route, Routes } from "react-router";
 import { Cart } from "./components/Cart";
+import { CartProvider } from "./service/CartContext";
 
 export default function App() {
 
-  const [cart, setCart] = useState([]);
-
-  function addToCart(product) {
-    setCart((prevCart) => [...prevCart, product]);
-  }
-
-   function updateQuantity(id, quantity) {
-    setCart(cart =>
-      cart.map(item =>
-        item.id === id ? { ...item, quantity: Math.max(1, quantity) } : item
-      )
-    );
-  }
-
-  function removeFromCart(id) {
-    setCart(cart => cart.filter(item => item.id !== id));
-  }
-
-  function clearCart() {
-    setCart([]);
-  }
   return (
-    //React Fragment
     <>
-      <Header cart={cart} />
-      <Routes>
-        <Route path="/" element={<ProductList addToCart={addToCart} />} />
-         <Route path="/cart" element={
-          <Cart 
-            cart={cart} 
-            updateQuantity={updateQuantity} 
-            removeFromCart={removeFromCart} 
-            clearCart={clearCart} 
-          />} 
-        />
-      </Routes>
+      <CartProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </CartProvider>
     </>
   );
 }
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,51 +1,43 @@
 import styles from "./Cart.module.css";
+import { useContext } from "react";
+import { CartContext } from "../service/CartContext";
 
-export function Cart({ cart, updateQuantity, removeFromCart, clearCart }) {
+export function Cart() {
+  const { cart, updateQtyCart, clearCart } = useContext(CartContext);
+
   return (
     <div className={styles.cart}>
       <h2>Shopping Cart</h2>
-      <button className={styles.clearBtn} onClick={clearCart}>
-        Remover todos
-      </button>
       {cart.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
         <ul>
           {cart.map((product, index) => (
-             <li key={index} className={styles.cartItem}>
+            <li key={index} className={styles.cartItem}>
               <img src={product.thumbnail} alt={product.title} />
-              <div className={styles.info}>
-                <h3>{product.title}</h3>
-                <p>R$ {product.price.toFixed(2)}</p>
-                <div className={styles.quantityControl}>
-                  <button
-                    onClick={() =>
-                      updateQuantity(product.id, product.quantity - 1)
-                    }
-                    disabled={product.quantity <= 1}
-                  >
-                    -
-                  </button>
-                  <span>{product.quantity}</span>
-                  <button
-                    onClick={() =>
-                      updateQuantity(product.id, product.quantity + 1)
-                    }
-                  >
-                    +
-                  </button>
-                </div>
+              <h3>{product.title}</h3>
+              <p>${product.price.toFixed(2)}</p>
+              <div className={styles.quantityControls}>
                 <button
-                  className={styles.removeBtn}
-                  onClick={() => removeFromCart(product.id)}
+                  onClick={() =>
+                    updateQtyCart(product.id, product.quantity - 1)
+                  }
                 >
-                  Remover
+                  -
+                </button>
+                <span>{product.quantity}</span>
+                <button
+                  onClick={() =>
+                    updateQtyCart(product.id, product.quantity + 1)
+                  }
+                >
+                  +
                 </button>
               </div>
             </li>
           ))}
         </ul>
-      )}
+        )}
     </div>
   );
 }
